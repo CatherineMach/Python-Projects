5765ff7dc61108243c27f1550ecf4e0a855df90b
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-05-18 13:51:03 -0500

    saving python work

---
+++ Python_If.py
@@ -0,0 +1,14 @@
+
+
+num1 = 13
+key = False
+
+if num1 == 12:
+    if key:
+        print ('Num1 is equal to Twelve and they have a key!')
+    else:
+        print ('Num1 is equal to Twelve and they do NOT have the key!')
+elif num1 < 12:
+    print ('Num1 is less than Twelve!')
+else:
+    print ('Num1 is NOT equal to Twelve!')
