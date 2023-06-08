e830a9ef016ad9d3bafd0a7d015f6adfd610914a
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-06-08 18:45:58 -0500

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -19,34 +19,37 @@ class Employee(User):
     department = "General"
     pin_number = "3980"
 
-#Child Class Customer
-class Customer(User):
-    code_number = "4321"           
-
-# This is the same method in the parent class "User".
-# The difference is that, instead of using entry_password, we're using entry_pin.
-
     def getLoginInfo(self):
         entry_name = input("Enter your name: ")
-        entry_email = input("Enter your email: ")
+        entry_email = input ("Enter your email: ")
         entry_pin = input("Enter your pin: ")
         if (entry_email == self.email and entry_pin == self.pin_number):
-            print("Welcome back, {}!".format(entry_name))
+            print ("Welcome back, {}!".format(entry_name))
         else:
-            print("The pin or email is incorrect")          
+            print ("The pin or email is incorrect")
 
+#Child Class Customer
+class Customer(User):
+    code_number = "4321"
+
+
+# This is the same method in the parent class "User"
+# The difference is that, instead of using entry_password, we're using entry_pin.
     def getLoginInfo(self):
         entry_name = input("Enter your name: ")
-        entry_code = input ("Enter the code: ")
+        entry_code = input("Enter the code: ")
         if (entry_name == self.name and entry_code == self.code_number):
             print("Hello {}! Welcome!".format(entry_name))
         else:
             print("The code seems to be wrong...")
 
-# The following code invokes the methods inside each class for User and Employee.
+# The following code invokes the method inside each class for User and Employee.
 
 customer = User()
 customer.getLoginInfo()
 
 manager = Employee()
 manager.getLoginInfo()
+
+customer2 = Customer()
+customer2.getLoginInfo()
