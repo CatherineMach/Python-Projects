e84fef0f50bc3e9ed0a4e36c010d72374d2a8467
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-06-08 18:57:43 -0500

    Update learningtkinter.py

--- learningtkinter.py
+++ learningtkinter.py
@@ -17,16 +17,32 @@ class ParentWindow(Frame):
         self.varLName = StringVar()
     
         self.lblFName = Label(self.master,text='First Name: ', font=("Helvetica", 16), fg='black', bg='lightblue' )
-        self.lblFName.grid(row=0, column=0)
+        self.lblFName.grid(row=0, column=0,padx=(30,0), pady=(30,0))
 
         self.lblLName = Label(self.master,text='Last Name: ', font=("Helvetica", 16), fg='black', bg='lightblue' )
-        self.lblLName.grid(row=0, column=0)
+        self.lblLName.grid(row=1, column=0,padx=(30,0), pady=(30,0))
+
+        self.lblDisplay = Label(self.master,text='', font=("Helvetica", 16), fg='black', bg='lightgray')
 
         self.lblFName = Entry(self.master,text=self.varFName, font=("Helvetica", 16), fg='black', bg='lightblue' )
-        self.lblFName.grid(row=1, column=0)
+        self.lblFName.grid(row=0, column=1,padx=(30,0), pady=(30,0))
 
         self.lblLName = Entry(self.master,text=self.varLName, font=("Helvetica", 16), fg='black', bg='lightblue' )
-        self.lblLName.grid(row=, column=)
+        self.lblLName.grid(row=1, column=1, padx=(30,0), pady=(30,0))
+
+        self.btnSubmit = Button(self.master, text="Submit", width=10, height=2,  command=self.submit)
+        self.btnSubmit.grid(row=2, column=1,padx=(0,90), pady=(30,0), sticky=NE)
+
+        self.btnCancel = Button(self.master, text="Cancel", width=10, height=2, command=self.cancel)
+        self.btnCancel.grid(row=2, column=1,padx=(0,90),pady=(30,0), sticky=NE)
+
+    def submit(self):
+        fn = self.varFName.get()
+        ln = self.varLName.get()
+        self.lblDisplay.config(text='Hello {} {}!'.format(fn,ln))
+
+    def cancel(self):
+        self.master.destroy()
 
 
 if __name__ == "__main__":
