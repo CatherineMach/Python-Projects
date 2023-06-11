d658f3575969eabff82eecd4fccd2b554473362f
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-06-11 17:43:39 -0500

    adding more codes

---
+++ encapsulate.py
@@ -0,0 +1,25 @@
+
+class Protected:
+    def __init__(self):
+        self._protectedVar = 0
+
+obj = Protected()
+obj._protectedVar = 34
+print(obj._protectedVar)
+
+
+class Protected2:
+    def __init__(self):
+        self.__privateVar = 12
+
+    def getPrivate(self):
+        print(self.__privateVar)
+
+    def setPrivate(self, private):
+        self.__privateVar = private
+
+obj = Protected2()
+obj.getPrivate()
+obj.setPrivate(23)
+obj.getPrivate()
+
