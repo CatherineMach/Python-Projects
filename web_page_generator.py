372c3762bf4ac96b5903529c716d17c397b1b3fb
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-06-22 14:25:22 -0500

    Update web_page_generator.py

--- web_page_generator.py
+++ web_page_generator.py
@@ -6,22 +6,32 @@ class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
+        
+        self.txtEntry = Entry(self.master)
+        self.txtEntry.grid(row=1, column=0, padx=(30,15), pady=(10,10), columnspan=3, sticky=W+E)
+
         self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
         self.btn.grid(padx=(10,10) , pady=(10,10))
-        self.txtEntry = Entry(self.master,)
-        self.txtEntry.grid(row=1, column=0, padx=(30,15), pady=(10,10), columnspan=3, sticky=W+E)
-        htmlText = self.txtEntry.get()
+
         self.btn = Button(self.master, text="customHTML", width=30, height=2, command=self.customHTML)
         self.btn.grid(padx=(10,10) , pady=(10,10))
+
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
-        htmlText = self.txtEntry.get() 
         htmlFile = open("index.html", "w")
         htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
         htmlFile.write(htmlContent)
         htmlFile.close()
         webbrowser.open_new_tab("index.html")
 
+    def customHTML(self):
+        htmlText = self.txtEntry.get()
+        htmlFile = open("index.html", "w")
+        htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
+        htmlFile.write(htmlContent)
+        htmlFile.close()
+        webbrowser.open_new_tab("index.html")
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
