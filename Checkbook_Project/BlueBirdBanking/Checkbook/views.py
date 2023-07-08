f34841bb79ebbec305d15f5637ca315a029e55e4
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-07-07 19:44:54 -0500

    fixed checkbook

--- Checkbook_Project/BlueBirdBanking/Checkbook/views.py
+++ Checkbook_Project/BlueBirdBanking/Checkbook/views.py
@@ -8,9 +8,11 @@ def home(request):
     form = TransactionForm(data=request.POST or None)
     # Checks if request method is POST
     if request.method == 'POST':
-        content = {'form': form}
+        pk = request.POST['account']
+        return balance(request, pk)
+    content = {'form': form}
     # Adds content of form to page
-    return render(request, 'checkbook/index.html')
+    return render(request, 'checkbook/index.html', content)
 
 
 # This function will render the Create New Account page when requested
@@ -23,7 +25,7 @@ def create_account(request):
             return redirect('index')  # Returns user back to the home page
     content = {'form': form}  # Saves content to the template as a dictionary
     # adds content of form to page
-    return render(request, 'checkbook/CreateNewAccount.html')
+    return render(request, 'checkbook/CreateNewAccount.html', content)
 
 
 # This function will render the Balance page when requested
@@ -41,7 +43,7 @@ def balance(request, pk):
             table_contents.update({t: current_total})
 
     content = {'account': account, 'table_contents': table_contents, 'balance': current_total}
-    return render(request, 'checkbook/BalanceSheet.html')
+    return render(request, 'checkbook/BalanceSheet.html', content)
 
 
 # This function will render the Transaction page when requested
@@ -50,10 +52,10 @@ def transaction(request):
     # Checks if request method is POST
     if request.method == 'POST':
         if form.is_valid():
-            pk = request.POST['account']# Check to see if the submitted form is valid and if so, saves the form
+            pk = request.POST['account'] # Check to see if the submitted form is valid and if so, saves the form
             form.save()    # Saves the transaction form
             return balance(request, pk)   # Redirects the user to the home page after form submission
     # Pass content to the template in a dictionary
     content = {'form': form}
     # Adds content of form to page
-    return render(request, 'checkbook/AddTransaction.html')
+    return render(request, 'checkbook/AddTransaction.html', content)
